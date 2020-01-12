8751b3ce534990a19378aefeb2758fa74fe82bea
Daria <dariagnaumova@gmail.com>
2020-01-11 20:18:41 -0500

    test of colorcoding

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var currentDate = moment().format('MMMM Do, YYYY');
 var now = moment();
 var schedule = {};
 
+
 // show current date on the page
 
 $("#currentDay").text(currentDate);
@@ -13,6 +14,7 @@ $('.saveBtn').on('click', function () {
     var time = $(this).parent().attr("data-time")
     // console.log(time + ": " + text)
     schedule[time] = text;
+    // console.log(schedule.hasOwnProperty('key'));
     // console.log(schedule);
     var schedString = JSON.stringify(schedule);
     localStorage.setItem('schedule', schedString)
@@ -29,35 +31,24 @@ if (storageSchedule) {
 }
 
 // colorcoding: change color of the textarea depending on it's time condition (past, present, or future).
-
 // using "for loop" for attributes and objects and classes defined with css
+
 for (var key in schedule) {
     // console.log(key, schedule[key])
+    // declare empty var to fill in with class later
     var timeClass = '';
 
     if (moment(key, 'H').isBefore(now, 'hour')) {
-        timeClass = "past"
+        timeClass = "past";
     }
     else if (moment(key, 'H').isAfter(now, 'hour')) {
         timeClass = "future"
     }
-    
+    else (moment(key, 'H').isSame(now, 'hour')) 
+        timeClass = "present"
+
     $("[data-time=" + key + "]")
-        .find('.description')
-        .val(schedule[key])
-        .addClass(timeClass)
+    .find('.description')
+    .val(schedule[key])
+    .addClass(timeClass);
 }
-
-
-
-// someFunction = () => {
-//     var items = []
-//     var currentDate = moment('12');
-//     new Array(48).fill().map((acc, index) => {
-//       items.push(currentDate.format('h:mm A'))
-//       currenDate = currentDate.add(30, 'minutes');
-//     })
-//     return items
-//   }
-
-//   console.log(someFunction())
